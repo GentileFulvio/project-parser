45b282703bee0f0ca9f288eae1aae9ce08c0e6c5
Fulvio Gentile <gentilefulvio1006@gmail.com>
2020-01-23 19:41:15 +0100

    Retrieves all package.json with filesystem

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
 mod util;
+use std::fs;
+
+fn find(location: &str) {
+    let dir = fs::read_dir(location).unwrap();
+    
+    for entry in dir {
+        let data = entry.unwrap();
+        let path = data.path();
+        let path_str = path.to_str().unwrap();
+
+        if path_str.contains("node_modules") {
+            continue
+        }
+
+        if data.file_type().unwrap().is_dir() {
+            find(path_str)
+        } else {
+            if path_str.contains("package.json") {
+                println!("{}", path_str)
+            }
+        }
+    }
+}
+
+fn find_by_filesystem() {
+    let cwd = util::get_working_dir().unwrap();
+
+    find(cwd.as_str());
+}
 
 fn main() {
-   util::search_by_glob();
+    find_by_filesystem();
 }
