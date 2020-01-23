42a6828e168fca52c43ed9b41ea51b51306f69ea
Fulvio Gentile <gentilefulvio1006@gmail.com>
2020-01-23 18:56:18 +0100

    Retrieves all package.json with globbing pattern (Super slow)

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,5 @@
-extern crate glob;
 mod util;
-use glob::{glob, MatchOptions, glob_with};
-use std::{thread, time};
-use std::sync::mpsc;
-use std::time::Duration;
-
-fn writeDataToFile(line: &str, file: &str) {
-
-}
-
-fn search() {
-    let pattern = format!("/**/package.json");
-    let options = MatchOptions{
-        case_sensitive: false,
-        require_literal_leading_dot: false,
-        require_literal_separator: false
-    };
-
-//    let (sender, receiver) = mspc::channel();
-//
-//    thread::Builder::new().name("child1".to_string()).spawn(move || {
-//
-//    });
-
-    let now = time::Instant::now();
-
-	for entry in glob_with(pattern.as_str(), options).expect("Failed to read glob pattern") {
-        thread::sleep(Duration::from_secs(1));
-        println!("{}", now.elapsed().as_secs());
-
-        match entry {
-            Ok(path) => {
-                match path.to_str() {
-                    Some(data) => {
-                        // if !data.contains("node_modules") && !data.starts_with("tools") {
-                            println!("{}", data)
-                        // }
-                    },
-                    None => println!("Unable to stringify buffer")
-                }
-            },
-            Err(e) => println!("{:?}", e),
-        }
-    }
-}
 
 fn main() {
-    match util::get_working_dir() {
-        Some(cwd) => {
-            println!("{}", cwd.as_str());
-        },
-        None => println!("Unable to retrieve current working directory")
-    }
-
-   search();
+   util::search_by_glob();
 }
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,4 +1,8 @@
+extern crate glob;
 use std::env;
+use glob::{glob};
+use std::{thread};
+use std::time::Duration;
 
 pub fn get_working_dir() -> Option<String> {
     match env::current_dir() {
@@ -14,6 +18,31 @@ pub fn get_working_dir() -> Option<String> {
             println!("{}", err);
 
             None
+        }
+    }
+}
+
+pub fn search_by_glob() {
+    let pattern = match get_working_dir() {
+        Some(str) => format!("{}/services/**/package.json", str),
+        None => format!("/services/**/package.json")
+    };
+
+    for entry in glob(pattern.as_str()).expect("Failed to read glob pattern") {
+        thread::sleep(Duration::from_millis(10));
+
+        match entry {
+            Ok(path) => {
+                match path.to_str() {
+                    Some(data) => {
+                        if !data.contains("node_modules") && !data.starts_with("tools") {
+                            println!("{}", data)
+                        }
+                    },
+                    None => println!("Unable to stringify buffer")
+                }
+            },
+            Err(e) => println!("{:?}", e),
         }
     }
 }
